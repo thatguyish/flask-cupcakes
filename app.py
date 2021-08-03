e566d3c8e66878ca667fa0bd39ccc7dffa7c317a
thatguyish <ishmell.ramirez@gmail.com>
2021-08-03 15:44:32 -0400

    completed models and attatched to main app

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 """Flask app for Cupcakes"""
+from flask import Flask
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+connect_db(app)
+
--- models.py
+++ models.py
@@ -1,1 +1,24 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer,primary_key=True,autoincrement = True)
+
+    flavor = db.Column(db.String(50),nullable = False)
+
+    size = db.Column(db.String(50),nullable = False)
+
+    rating = db.Column(db.Float,nullable = False)
+
+    image = db.Column(db.String(200),default="https://tinyurl.com/demo-cupcake",nullable = False)
+
+
