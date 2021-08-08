21501b68da919cb6ec5118fff3b4291cd3549eb3
thatguyish <ishmell.ramirez@gmail.com>
2021-08-07 22:05:16 -0400

    finished part 3 added patch and delete to rest api

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
 """Flask app for Cupcakes"""
-from flask import Flask
+from flask import Flask, json, jsonify, request
 from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
 connect_db(app)
 
+@app.route('/api/cupcakes')
+def get_all_cupcakes():
+    all_cupcakes = Cupcake.query.all()
+    cupcakes = [cupcake.serialize() for cupcake in all_cupcakes]
+    return jsonify(cupcakes=cupcakes)
+
+@app.route('/api/cupcakes',methods=["POST"])
+def add_cupcake():
+    cupcake = Cupcake(flavor=request.json['flavor'],size=request.json['size'],rating=request.json['rating'],image=request.json['image'],)
+    db.session.add(cupcake)
+    db.session.commit()
+    return jsonify(cupcake=cupcake.serialize()), 201
+
+@app.route('/api/cupcakes/<int:id>')
+def get_cupcake(id):
+    cupcake = Cupcake.query.get_or_404(id)
+
+    return jsonify(cupcake=cupcake.serialize())
+
+@app.route('/api/cupcakes/<int:id>',methods=["PATCH"])
+def update_cupcake(id):
+    cupcake = Cupcake.query.get_or_404(id)
+
+    cupcake.flavor = request.json.get('flavor',cupcake.flavor)
+    cupcake.image = request.json.get('image',cupcake.image)
+    cupcake.rating = request.json.get('rating',cupcake.rating)
+    cupcake.size = request.json.get('size',cupcake.size)
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    return cupcake.serialize()
+
+@app.route('/api/cupcakes/<int:id>',methods=["DELETE"])
+def delete_cupcake(id):
+    cupcake = Cupcake.query.get_or_404(id)
+    db.session.delete(cupcake)
+    db.session.commit()
+    return {"message":"Deleted"}
--- models.py
+++ models.py
@@ -21,4 +21,13 @@ class Cupcake(db.Model):
 
     image = db.Column(db.String(200),default="https://tinyurl.com/demo-cupcake",nullable = False)
 
+    def serialize(self):
+        return {
+            'id':self.id,
+            'flavor':self.flavor,
+            'size':self.size,
+            'rating':self.rating,
+            'image':self.image
+        }
 
+    
